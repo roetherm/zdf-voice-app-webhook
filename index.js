96ebefc6f928cd3021793a73731d74517f832490
MICHAEL-PC\micha <michaelroether@gmx.net>
2017-12-07 16:12:42 +0100

    testing

--- index.js
+++ index.js
@@ -12,15 +12,26 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/info', function(req, res) {
-    var forename = req.body.result && req.body.result.parameters && req.body.result.parameters.playerforename ? req.body.result.parameters.playerforename : "Kein Vorname."
-    var lastname = req.body.result && req.body.result.parameters && req.body.result.parameters.playerlastname ? req.body.result.parameters.playerlastname : "Kein Nachname."
-    var information = req.body.result && req.body.result.parameters && req.body.result.parameters.information ? req.body.result.parameters.information : "Keine Information."
-    var teamname = req.body.result && req.body.result.parameters && req.body.result.parameters.teamname ? req.body.result.parameters.teamname : "Kein Teamname."
+    var forename = req.body.result && req.body.result.parameters && req.body.result.parameters.playerforename ? req.body.result.parameters.playerforename : "Kein Vorname.";
+    var lastname = req.body.result && req.body.result.parameters && req.body.result.parameters.playerlastname ? req.body.result.parameters.playerlastname : "Kein Nachname.";
+    var information = req.body.result && req.body.result.parameters && req.body.result.parameters.information ? req.body.result.parameters.information : "Keine Information.";
+    var teamname = req.body.result && req.body.result.parameters && req.body.result.parameters.teamname ? req.body.result.parameters.teamname : "Kein Teamname.";
     console.log("test");
     return res.json({
         speech: forename + " " + lastname + ". Information: " + information + ". Team: " + teamname + ".",
         displayText: forename + " " + lastname + ". Information: " + information + ". Team: " + teamname + ".",
         source: 'webhook-echo-sample'
+    });
+});
+
+restService.post('https://zdf-voice-app.herokuapp.com/playerInfo', function(req, res) {
+    var forename = req.body.result && req.body.result.parameters && req.body.result.parameters.playerforename ? req.body.result.parameters.playerforename : "Kein Vorname."
+    var lastname = req.body.result && req.body.result.parameters && req.body.result.parameters.playerlastname ? req.body.result.parameters.playerlastname : "Kein Nachname."
+    var information = req.body.result && req.body.result.parameters && req.body.result.parameters.information ? req.body.result.parameters.information : "Keine Information."
+    var teamname = req.body.result && req.body.result.parameters && req.body.result.parameters.teamname ? req.body.result.parameters.teamname : "Kein Teamname."
+    return res.json({
+        forename: forename,
+        lastname: lastname
     });
 });
 
