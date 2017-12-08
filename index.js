a61a02d98f3ab2f27252af6d803e311171b198df
MICHAEL-PC\micha <michaelroether@gmx.net>
2017-12-08 12:07:58 +0100

    testing

--- index.js
+++ index.js
@@ -3,27 +3,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const restService = express();
+const app = express();
 
-restService.use(bodyParser.urlencoded({
+app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-restService.use(bodyParser.json());
+app.use(bodyParser.json());
 
-restService.post('/info', function(req, res) {
-    var forename = req.body.result && req.body.result.parameters && req.body.result.parameters.playerforename ? req.body.result.parameters.playerforename : "Kein Vorname.";
-    var lastname = req.body.result && req.body.result.parameters && req.body.result.parameters.playerlastname ? req.body.result.parameters.playerlastname : "Kein Nachname.";
-    var information = req.body.result && req.body.result.parameters && req.body.result.parameters.information ? req.body.result.parameters.information : "Keine Information.";
-    var teamname = req.body.result && req.body.result.parameters && req.body.result.parameters.teamname ? req.body.result.parameters.teamname : "Kein Teamname.";
-    console.log("test");
-    return res.json({
+app.get('/info', function(request, response) {
+  response.send("Hello, im in.");
+})
+
+app.post('/info', function(request, response) {
+    var forename = request.body.result && request.body.result.parameters && request.body.result.parameters.playerforename ? request.body.result.parameters.playerforename : "Kein Vorname.";
+    var lastname = request.body.result && request.body.result.parameters && request.body.result.parameters.playerlastname ? request.body.result.parameters.playerlastname : "Kein Nachname.";
+    var information = request.body.result && request.body.result.parameters && request.body.result.parameters.information ? request.body.result.parameters.information : "Keine Information.";
+    var teamname = request.body.result && request.body.result.parameters && request.body.result.parameters.teamname ? request.body.result.parameters.teamname : "Kein Teamname.";
+    return response.json({
         speech: forename + " " + lastname + ". Information: " + information + ". Team: " + teamname + ".",
         displayText: forename + " " + lastname + ". Information: " + information + ". Team: " + teamname + ".",
         source: 'webhook-echo-sample'
     });
 });
 
-restService.listen((process.env.PORT || 8000), function() {
+app.listen((process.env.PORT || 8000), function() {
     console.log("Server up and listening");
 });
